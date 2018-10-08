5957bdaa9c02164b605d577d4fa913c3e9acca54
mdb <mdb@protonmail.ch>
2018-10-08 22:25:17 +0800

    Main menu tweaks

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-const MainMenu = (props) => (
-    <nav role='navigation' className="main-nav" id="main-nav">
-        <ul id="main-nav-list">
-            {props.data.length > 1 && props.data.map((m) =>
-                (<li key={m.id}><Link to={(m.slug === "home" ? "/" : "/" + m.slug)}>{m.name}</Link></li>)
-            )}
-        </ul>
-    </nav>
-)
+const MainMenu = (props) => {
+
+    return (
+        <nav role='navigation' className="main-nav" id="main-nav">
+            <ul id="main-nav-list">
+                {props.data.length > 1 && props.data.map((m) =>
+                    (
+                        <li key={m.id}>
+                            <Link to={(m.slug === "home" ? "/" : "/" + m.slug)}
+                                className={props.location.pathname.includes(m.slug)
+                                    || (props.location.pathname === "/" && m.slug === "home") ? "active" : ""}>
+                                {m.name}
+                            </Link>
+                        </li>
+                    )
+                )}
+            </ul>
+        </nav>
+    )
+}
 
 const mapStateToProps = (state) => {
     return ({
--- src/reducer.js
+++ src/reducer.js
@@ -9,7 +9,7 @@ const initialState = {
 
 export const getData = () => {
   return dispatch => {
-    axios.get('https://wp.paulabramwell.com.au/wp-json/wp/v2/pages?order=asc')
+    axios.get('https://wp.paulabramwell.com.au/wp-json/wp/v2/pages?orderby=menu_order&order=asc')
       .then(function (response) {
         return dispatch({
           type: GET_DATA,
