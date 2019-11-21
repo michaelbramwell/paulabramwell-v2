e6dbc4d0bf137095708aa365bcc1a2078659be08
mdb <mdb@protonmail.ch>
2019-11-21 22:30:53 +0800

    Filtered wp menu response into structured parent child relationships

--- src/reducer.js
+++ src/reducer.js
@@ -11,15 +11,31 @@ export const getData = () => {
   return dispatch => {
     axios.get('https://wp.paulabramwell.com.au/wp-json/wp/v2/pages?orderby=menu_order&order=asc')
       .then(function (response) {
+        const unstructured = response.data.map((m) => ({
+          name: m.title.rendered,
+          slug: m.slug,
+          link: m.link,
+          content: m.content.rendered,
+          id: m.id,
+          parent: m.parent,
+          children: [{}]
+        }));
+        const parentData = unstructured.filter((u) => u.parent === 0); 
+        const childData = unstructured.filter((u) => u.parent > 0); 
+        
+        const combined = parentData.map((p) => {
+          const children = childData.filter(c => c.parent === p.id);
+          
+          return {
+            ...p,
+            children: children
+          }
+        });
+        console.log(combined)
+
         return dispatch({
           type: GET_DATA,
-          value: response.data.map((m) => ({
-            name: m.title.rendered,
-            slug: m.slug,
-            link: m.link,
-            content: m.content.rendered,
-            id: m.id
-          }))
+          value: combined
         })
       })
       .catch(function (error) {
