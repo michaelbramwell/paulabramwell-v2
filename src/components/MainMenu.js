a519969d985058f5a4ab7562c65290c86d20a07a
mdb <mdb@protonmail.ch>
2020-02-18 14:02:29 +0800

    Added FA icon and merged post arr into pages arr

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -2,28 +2,40 @@ import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { slide as Menu } from 'react-burger-menu'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolderMinus, faFolderPlus } from '@fortawesome/free-solid-svg-icons'
 
 const MainMenu = (props) => {
-    const [isOpen, setOpen] = useState(0);
+    const [isOpen, setOpen] = useState(false);
 
-    const { posts } = props
+    const pagesAndBlog = (arr, index, ...newItems) => [
+        ...props.data.slice(0, 2),
+        ...[{
+            id: 999,
+            name: 'Blog',
+            slug: 'blog'
+        }],
+        ...props.data.slice(2)
+      ]
 
     const subMenu = (parentSlug, children) => {
 
-        if (!children || children.length === 0) return <></>
+        if (!children || children.length === 0 || !isOpen) return <></>
 
         return (
             <div>
                 {
                     children.map((m) => {
-                        return (<div key={m.id}>
-                            <Link to={"/" + parentSlug + '/' + m.slug}
-                                onClick={() => setOpen(0)}
-                                className={props.location.pathname.includes(m.slug)
-                                    ? "active" : ""}>
-                                {m.name}
-                            </Link>
-                        </div>
+                        return (
+                            <div key={m.id}>
+
+                                <Link to={"/" + parentSlug + '/' + m.slug}
+                                    onClick={() => setOpen(0)}
+                                    className={props.location.pathname.includes(m.slug)
+                                        ? "active" : ""}>
+                                    {m.name}
+                                </Link>
+                            </div> 
                         )
                     })
                 }
@@ -33,32 +45,33 @@ const MainMenu = (props) => {
 
     return (
         <nav role='navigation' className="main-nav" id="main-nav">
-            <Menu id="main-nav-list" isOpen={isOpen}>
-                {props.data.length > 0 && props.data.map((m) => {
+            <Menu id="main-nav-list">
+                {props.data.length > 0 && pagesAndBlog().map((m) => {
                     return (
-                        <Link
-                            key={m.id}
-                            to={(m.slug === "home" ? "/" : "/" + m.slug)}
-                            onClick={() => setOpen(isOpen - 1)}
-                            className={props.location.pathname.includes(m.slug)
-                                || (props.location.pathname === "/" && m.slug === "home") 
-                                ? "menu-item active" 
-                                : "menu-item"}>
-                            {m.name}
-
-                            {subMenu(m.slug, m.children)}
-                        </Link>
+                        <div>
+                            <Link
+                                key={m.id}
+                                to={(m.slug === "home" ? "/" : "/" + m.slug)}
+                                onClick={() => setOpen(m.children && m.children.length > 0 ? !isOpen : isOpen)}
+                                className={props.location.pathname.includes(m.slug)
+                                    || (props.location.pathname === "/" && m.slug === "home") 
+                                    ? "menu-item active" 
+                                    : "menu-item"}>
+                                {m.name}
+                                
+                                {m.children && m.children.length > 0 && 
+                                    <FontAwesomeIcon 
+                                        style={{marginLeft: '5px'}} 
+                                        icon={isOpen ? faFolderMinus : faFolderPlus} 
+                                    />
+                                }
+                                
+                                {subMenu(m.slug, m.children)}
+                            </Link>
+                        </div>
                     )
                 }
                 )}
-
-                {posts && posts.length > 0 &&
-                    <Link key={100}
-                        to={"/blog"}
-                        className={props.location.pathname === "/blog" ? "menu-item active" : "menu-item"}>
-                        Blog
-                    </Link>
-                }
             </Menu>
         </nav>
     )
